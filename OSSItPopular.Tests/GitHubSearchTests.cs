681d9cd3d683f8f5a2460366a86e255bac875fbd
marcusoft <marcusoft.net@gmail.com>
2013-08-15 13:48:45 +0200

    Added the first test

---
+++ OSSItPopular.Tests/GitHubSearchTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace OSSItPopular.Tests
+{
+    public class GitHubSearchTests
+    {
+        [Fact]
+        public void ShouldSearchRepositoriesByName()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            throw new NotImplementedException("Implement test");
+        }
+    }
+}
+

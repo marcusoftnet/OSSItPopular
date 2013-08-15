d21b5e4e5c3d8d7bdc8ee509063a81eb1a2da5b7
marcusoft <marcusoft.net@gmail.com>
2013-08-15 13:36:01 +0200

    Got inital structure and housekeeping in place

---
+++ SolutionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("www.marcusoft.net (c)")]
+[assembly: AssemblyProduct("OSSItPopular.Web")]
+[assembly: AssemblyCopyright("Copyright © Marcus Hammarberg  2013")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

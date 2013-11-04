5c1c804589589d903d217aa117bac6cc97b7e966
Marcus Hammarberg <marcusoft.net@gmail.com>
2013-11-04 20:59:07 +0100

    Added first TwitterClient implementation

---
+++ OSSItPopular.Web/Models/TwitterSearchResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OSSItPopular.Web.Models
+{
+    public class TwitterSearchResult
+    {
+        public string ID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Text { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ OSSItPopular.Web/Modules/TwitterModule.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using LinqToTwitter;
+using Nancy;
+using OSSItPopular.Web.Models;
+
+namespace OSSItPopular.Web.Modules
+{
+    public class TwitterModule : NancyModule
+    {
+
+        public TwitterModule()
+        {
+            Get["/twitter/{search}"] = parameters =>
+            {
+                string searchterm = parameters.Search;
+
+                var result = (from search in CurrentTwitterContext.Search
+                              where search.Type == SearchType.Search &&
+                                    search.Query == searchterm
+                              select search)
+                            .SingleOrDefault();
+
+                var resultList = result.Statuses.Select(
+                    status => new TwitterSearchResult
+                    {
+                        ID = status.ID,
+                        Link = status.Source,
+                        CreatedAt = status.CreatedAt,
+                        Text = status.Text
+                    });
+
+                return resultList.ToList();
+            };
+        }
+
+        private TwitterContext _twitterContext;
+
+        private TwitterContext CurrentTwitterContext
+        {
+            get { return _twitterContext ?? (_twitterContext = new TwitterContext(Authorizer)); }
+        }
+
+        private SingleUserAuthorizer Authorizer
+        {
+            get
+            {
+                return new SingleUserAuthorizer
+                {
+                    Credentials = new SingleUserInMemoryCredentials
+                    {
+                        ConsumerKey = ConfigurationManager.AppSettings["twitterConsumerKey"],
+                        ConsumerSecret = ConfigurationManager.AppSettings["twitterConsumerSecret"],
+                        TwitterAccessToken = ConfigurationManager.AppSettings["twitterAccessToken"],
+                        TwitterAccessTokenSecret = ConfigurationManager.AppSettings["twitterAccessTokenSecret"]
+                    }
+                };
+            }
+        }
+    }
+}
